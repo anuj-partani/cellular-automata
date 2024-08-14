6698b952221bd8f529e58b52467fb7bd4d95b770
anuj-partani <partaniar@gmail.com>
2024-08-14 18:18:18 +0530

    Add files via upload

---
+++ rule110.v
@@ -0,0 +1,15 @@
+
+module rule110(
+    input clk,
+    input load,
+    input [511:0] in_data,
+    output reg [511:0] out_data ); 
+    
+    always @(posedge clk)
+        
+        if(load)
+            out_data <= in_data;    
+    	else
+            out_data <= (out_data ^ {out_data[510:0],1'b0}) | (~{1'b0,out_data[511:1]} & out_data);
+
+endmodule
---
+++ rule30.v
@@ -0,0 +1,15 @@
+
+module rule30(
+	input clk,
+	input load,
+	input [511:0] in_data,
+	output reg [511:0] out_data
+);	
+
+	always @(posedge clk)
+		if (load)
+			out_data <= in_data;
+		else
+			out_data <= {1'b0,out_data[511:1]} ^ (out_data[511:0] | {out_data[510:0],1'b0});
+		
+endmodule
